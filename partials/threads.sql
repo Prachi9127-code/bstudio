fd96526782e75a7f183375cadd26ec582def4b6f
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-07 16:31:08 +0530

    Update threads.sql

--- partials/threads.sql
+++ partials/threads.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `threads` (
-    `thread_id` int(7) NOT NULL,
+    `thread_id` int(7) NOT NULL AUTO_INCREMENT,
     `thread_title` varchar(255) NOT NULL,
     `thread_desc` text NOT NULL,
     `thread_cat_id` int(7) NOT NULL,
     `thread_user_id` int(7) NOT NULL,
-    `timestamp` datetime NOT NULL DEFAULT current_timestamp()
+    `timestamp` datetime NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`sno`)
 );
