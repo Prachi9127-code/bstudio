c32e8dbad80d6b44d9b83d855fb0be6edbab1022
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-07 16:31:51 +0530

    Update comment.sql

--- partials/comment.sql
+++ partials/comment.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `comment` (
-    `comment_id` int(11) NOT NULL,
+    `comment_id` int(11) NOT NULL AUTO_INCREMENT,
     `comment_content` text NOT NULL,
     `thread_id` int(11) NOT NULL,
     `comment_by` int(8) NOT NULL,
-    `comment_time` datetime NOT NULL DEFAULT current_timestamp()
+    `comment_time` datetime NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`sno`)
 );
