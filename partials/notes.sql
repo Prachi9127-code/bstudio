3f835e971672ae6ae785725666fee753f6f93534
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-07 14:14:32 +0530

    Update notes.sql

--- partials/notes.sql
+++ partials/notes.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `note` (
-    `sno` int(11) NOT NULL DEFAULT '2' AUTO_INCREMENT,
+    `sno` int(11) NOT NULL AUTO_INCREMENT,
     `title` varchar(50) NOT NULL,
     `description` text NOT NULL,
     `tstamp` datetime NOT NULL DEFAULT current_timestamp()
