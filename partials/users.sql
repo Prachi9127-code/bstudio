d6b2525f6abeaee8c419a54a60a51ae0091a2431
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-07 16:27:45 +0530

    Update users.sql

--- partials/users.sql
+++ partials/users.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `users` (
-    `sno` int(20) NOT NULL,
+    `sno` int(20) NOT NULL AUTO_INCREMENT,
     `username` varchar(50) NOT NULL,
     `password` varchar(255) NOT NULL,
-    `date` datetime NOT NULL DEFAULT current_timestamp()
+    `date` datetime NOT NULL DEFAULT current_timestamp(),
+     PRIMARY KEY (`sno`)
 );
