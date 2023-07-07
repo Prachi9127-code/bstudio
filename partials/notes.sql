c576232d4adef67a0d97613a81795ac95d6032a9
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-07 11:13:31 +0530

    notes.sql

--- partials/notes.sql
+++ partials/notes.sql
@@ -2,5 +2,6 @@ CREATE TABLE `note` (
     `sno` int(11) NOT NULL,
     `title` varchar(50) NOT NULL,
     `description` text NOT NULL,
-    `tstamp` datetime NOT NULL DEFAULT current_timestamp()
+    `tstamp` datetime NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`sno`)
 );
