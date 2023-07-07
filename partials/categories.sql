923372c15052954da902ce49d1333a57eaf0d615
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-07 16:32:20 +0530

    Update categories.sql

--- partials/categories.sql
+++ partials/categories.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `categories` (
-    `category_id` int(8) NOT NULL,
+    `category_id` int(8) NOT NULL AUTO_INCREMENT,
     `category_name` varchar(255) NOT NULL,
     `category_description` varchar(255) NOT NULL,
-    `created` datetime NOT NULL DEFAULT current_timestamp()
+    `created` datetime NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`sno`)
 );
