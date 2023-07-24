11cb3cabf3fe4ec2f9931c1ba524aac8b790deae
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-24 21:17:48 +0530

    Update login.php

--- login.php
+++ login.php
@@ -24,7 +24,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 }
             }
         }else{
-        $showError = "Invalid Credential";
+        $showError = "Invalid Credentials";
     }
 }
 ?>
