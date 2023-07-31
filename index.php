c348d08f883eabe7e3d476e7f651c63fe48d5b2f
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-31 20:03:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-session_start();
-
-if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
-    header("location: login.php");
-    exit;
-}
 
 //connect to the Database
 $insert = false;
