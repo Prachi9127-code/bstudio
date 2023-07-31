bc383eba1f2abdf34e56692773924da94c4b851f
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-31 16:42:48 +0530

    Update login.php

--- login.php
+++ login.php
@@ -14,7 +14,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             while($row=mysqli_fetch_assoc($result)){
                 if(password_verify($password , $row['password'])){
                 $login = true;
-                header("location: index.php");
                 session_start();
                 $_SESSION['loggedin'] = true;
                 $_SESSION['sno'] = $row['sno'];
