35e1326bc6a7ca61a87e7d2a3ba8d91f4eedecfa
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-31 15:37:42 +0530

    Update login.php

--- login.php
+++ login.php
@@ -15,8 +15,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 if(password_verify($password , $row['password'])){
                 $login = true;
                 session_start();
-                $_SESSION['loggedin'] = true;
-                //$_SESSION['sno'] = $row['sno'];
                 $_SESSION['username'] = $username;
                 header("location: index.php");
                 }else{
