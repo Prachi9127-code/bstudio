710dadc32c0123bdfdf3218ffc451f2b5278fbc9
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-31 15:35:36 +0530

    Update login.php

--- login.php
+++ login.php
@@ -14,9 +14,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             while($row=mysqli_fetch_assoc($result)){
                 if(password_verify($password , $row['password'])){
                 $login = true;
-                //session_start();
+                session_start();
                 $_SESSION['loggedin'] = true;
-                $_SESSION['sno'] = $row['sno'];
+                //$_SESSION['sno'] = $row['sno'];
                 $_SESSION['username'] = $username;
                 header("location: index.php");
                 }else{
